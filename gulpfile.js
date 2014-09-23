4f02baea43b99ebe19b1617a3b6dfb98f529de2b
krazemon <stephenmalina@gmail.com>
2014-09-23 01:27:25 -0400

    Get scaffolding working
    - Concatenate frontend js files into build directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,33 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    browserify = require('gulp-browserify'),
+    reactify = require('reactify'),
+    rename = require('gulp-rename'),
+    connect = require('gulp-connect');
 
-gulp.task('default', function() {
-
+gulp.task('html', function() {
+  gulp.src('index.html')
+      .pipe(gulp.dest('build'));
 });
+
+gulp.task('build', ['html'], function() {
+  // Single entry to browserify
+  gulp.src('js/app.react')
+      .pipe(browserify({
+        insertGlobals: true,
+        debug: !gulp.env.production,
+        extensions: ['.react'],
+        transform: [reactify]
+      }))
+      .pipe(rename('app.js'))
+      .pipe(gulp.dest('./build'))
+});
+
+gulp.task('frontendServer', ['build'], function() {
+  connect.server({
+    root: 'build',
+    livereload: true
+  });
+});
+
+gulp.task('default', ['build', 'frontendServer']);
