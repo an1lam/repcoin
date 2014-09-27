16d3f8ff333923630e0fb82fe2575d7a295a3b65
krazemon <stephenmalina@gmail.com>
2014-09-27 16:22:26 -0400

    Style categories list

--- js/components/Category.jsx
+++ js/components/Category.jsx
@@ -7,14 +7,16 @@ var Link = require('react-router').Link;
 var Category = React.createClass({
   render: function() {
     return (
-<div><h3><Link to="category" params={{category: this.props.category}}>{this.props.category}</Link></h3>
+      <div className="col-md-4">
+        <h3><Link to="category" params={{category: this.props.category}}>{this.props.category}</Link></h3>
+        <strong>Quotes:</strong>
       {this.formatQuotes()}</div>
     );
   },
 
   formatQuotes: function() {
     var formattedQuotes = this.props.quotes.map(function(quote) {
-      return <p>{quote}</p>;
+      return <p>"{quote}"</p>;
     });
     return formattedQuotes;
   }
