e118152a46948507bb5293b6356d70e15624bc0a
ritmatter <mritter123@gmail.com>
2014-09-28 22:51:06 -0400

    Flesh out profile page

--- js/components/ProfileBox.jsx
+++ js/components/ProfileBox.jsx
@@ -2,17 +2,18 @@
 "use strict";
 
 var React = require('react');
+var Feed = require('./Feed');
 
 var ProfileBox = React.createClass({
   render: function() {
     return (
       <div className="profileBox">
-        {this.props.user.name}<br />
-        <img className="profilePicture" src={this.props.user.picture}></img>
+        <img className="profilePicture img-responsive img-thumbnail" src={this.props.user.picture}></img>
         <div className="profileData">
-          Username: {this.props.user.username}<br />
-          Github: {this.props.user.github}<br />
-          Website: {this.props.user.website}<br />
+          <h3 className="profileUsername">{this.props.user.name}</h3>
+          <div className="username"><h4>Username: {this.props.user.username}</h4></div>
+          <div className="github"><h4>Github: {this.props.user.github}</h4></div>
+          <div className="website"><h4>Website: {this.props.user.website}</h4></div>
         </div>
       </div>
     );
