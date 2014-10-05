bc341a3933bd20f8126e87f439f25cb9086240c0
Stephen Malina <krazemon@users.noreply.github.com>
2014-10-05 17:19:54 -0400

    Update User.js
    Fixed spacing and if () return

--- api/models/User.js
+++ api/models/User.js
@@ -2,43 +2,49 @@
 // code used from MongoDB tutorial: http://blog.mongodb.org/post/32866457221/password-authentication-with-mongoose-part-1
 
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema,
-    bcrypt = require('bcrypt'),
-    SALT_WORK_FACTOR = 10;
+  Schema = mongoose.Schema,
+  bcrypt = require('bcrypt'),
+  SALT_WORK_FACTOR = 10;
 
 var UserSchema = new Schema({
-    username: {type: String, required: true, index: { unique: true } },
-    password: {type: String, required: true },
-    phoneNumber: {type: String, required: true, uniqe: true },
-    timeStamp : {type: Date, default: Date.now }
+  username: {type: String, required: true, index: { unique: true } },
+  password: {type: String, required: true },
+  phoneNumber: {type: String, required: true, unique: true },
+  timeStamp : {type: Date, default: Date.now }
 });
 
-
+// TODO: Abstract this logic out into it's own function for testing purposes
 UserSchema.pre('save', function(next) {
-    var user = this;
+  var user = this;
 
-    // Only hash the password if it has been modified (or is new)
-    if (!user.isModified('password')) return next();
+  // Only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
 
-    // generate a salt
-    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-        if (err) return next(err);
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
 
-        // hash the password along with our new salt
-        bcrypt.hash(user.password, salt, function(err, hash) {
-        if (err) return next(err);
+    // hash the password along with our new salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) {
+          return next(err);
+        }
 
-            // Override the cleartext password with the hashed one
-            user.password = hash;
-            next();
-        });
+        // Override the cleartext password with the hashed one
+        user.password = hash;
+        next();
     });
+  });
 });
 
 UserSchema.methods.comparePassword = function(candidatePassword, cb) {
     bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-        if (err) return cb(err);
-        cb(null, isMatch);
+      if (err) {
+        return cb(err);
+      }
+      cb(null, isMatch);
     });
 };
 
