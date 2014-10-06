b753e8ee485ad3653a49fff6ed672c5a8e1efdcb
ritmatter <mritter123@gmail.com>
2014-10-06 19:27:06 -0400

    Add loggedin route

--- api/routes/AuthRoutes.js
+++ api/routes/AuthRoutes.js
@@ -2,6 +2,11 @@
 // Routes to login, logout, and signup
 
 module.exports = function(router, passport) {
+  router.route('/loggedin')
+    .get(function(req, res) {
+      res.send(req.isAuthenticated());
+    });
+
   router.route('/login')
     .post(passport.authenticate('local'), function(req, res) {
        var user = req.user || {};
