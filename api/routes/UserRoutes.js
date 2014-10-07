4e98c5ac7f7fb60a4502bf30b1ffe22f8471d06d
ritmatter <mritter123@gmail.com>
2014-10-07 10:19:52 -0400

    Add crud operations for user

--- api/routes/UserRoutes.js
+++ api/routes/UserRoutes.js
@@ -38,4 +38,50 @@ module.exports = function(router) {
         }  
       });
     });
+
+/////// Routes that have /users/:user_id ///////////
+
+    // Get the user with the provided id
+  router.route('/users/:user_id')
+    .get(function(req, res) {
+      User.findById(req.params.user_id, function(err, user) {
+        if (err){
+          res.send(err);
+        } else {
+          res.send(user);
+        }
+      });
+    })
+
+    // Update the user with this id
+    .put(function(req, res) {
+      User.findById(req.params.user_id, function(err, user) {
+        if (err) {
+          res.send(err);
+        } else {
+          user.username    = req.body.username;
+          user.password    = req.body.password;
+          user.phoneNumber = req.body.phoneNumber;
+          user.save(function(err) {
+            if (err) {
+              res.send(err);
+            } else {
+              res.send(user);
+            }
+          });
+        }
+      });
+    })
+
+   // Delete the user with this id
+   .delete(function(req, res) {
+      // Remove the user
+      User.remove({ _id: req.params.user_id }, function(err, user) {
+        if (err) {
+          res.send(err);
+        } else {
+          res.json({message: 'Successfully deleted user' });
+        }
+      });
+   });
 };
