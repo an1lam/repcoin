f74ce8f3f045139a67020439f2956b184e697c19
ritmatter <mritter123@gmail.com>
2014-10-07 16:34:36 -0400

    Add basic http get request for loggedin route

--- test/app_test.js
+++ test/app_test.js
@@ -15,7 +15,6 @@ function defaultGetOptions(path) {
 }
 
 describe('app', function () {
- 
   before (function (done) {
     server = app.listen(port, function (err, result) {
       if (err) {
@@ -43,5 +42,20 @@ describe('app', function () {
       done();
     });
   });
- 
+
+  describe('authentication routes', function() {
+    it('should return false for loggedin before logging in', function(done) {
+      var headers = defaultGetOptions('/api/loggedin');
+      http.get(headers, function(res) {
+        var body = '';
+        res.on('data', function(chunk) {
+          body += chunk;
+        });
+        res.on('end', function() {
+          body.should.equal('false');
+          done();
+        });
+      });
+    });
+  });
 });
