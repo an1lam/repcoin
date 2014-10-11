49f0eb7ec28997881ae96b1ae8ff41e1e1cc9464
Matt Ritter <Matthew.P.Ritter.15@Dartmouth.edu>
2014-10-11 17:43:21 -0400

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,9 +1,7 @@
-module.exports = {
-  function(req, res, next) {
-    if (!req.isAuthenticated()) {
-        res.status(401).end();
-    } else {
-      next();
-    }
+module.exports = function(req, res, next) {
+  if (!req.isAuthenticated()) {
+    res.status(401).end();
+  } else {
+    next();
   }
 }
