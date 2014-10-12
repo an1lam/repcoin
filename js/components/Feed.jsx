04fcb693537f9348b2b658549fdf3e1dfeb087d7
ritmatter <mritter123@gmail.com>
2014-10-12 15:46:32 -0400

    Revert unnecessary change

--- js/components/Feed.jsx
+++ js/components/Feed.jsx
@@ -5,35 +5,106 @@ var React = require('react');
 var FeedItem = require('./FeedItem.jsx');
 var FeedHeader = require('./FeedHeader.jsx');
 var $ = require('jquery');
+var mockDonor = {
+  "id": "123",
+  "name": "Stephen Malina"
+};
+
+var mockDonor2 = {
+  "id": "123",
+  "name": "Will Baird"
+};
+
+var mockDonor3 = {
+  "id": "284",
+  "name": "Taylor Malmsheimer"
+};
+
+var mockReceiver = {
+  "id": "456",
+  "name": "Matt Ritter"
+};
+
+var mockReceiver2 = {
+  "id": "999",
+  "name": "Andrew Lindner"
+};
+
+var mockReceiver3 = {
+  "id": "133",
+  "name": "Aaron Lewin"
+};
+
+
+var mockAmount = 100;
+var mockAmount2 = 200;
+var mockAmount3 = 987;
+
+var mockCategory = "Coding";
+var mockCategory2 = "Reading";
+var mockCategory3 = "Skiing";
+
+var mockFeedItems = [
+  {
+    "from": mockDonor,
+    "to": mockReceiver,
+    "amount": mockAmount,
+    "category": mockCategory
+  },
+  {
+    "from": mockDonor2,
+    "to": mockReceiver2,
+    "amount": mockAmount2,
+    "category": mockCategory2
+  },
+  {
+    "from": mockDonor3,
+    "to": mockReceiver3,
+    "amount": mockAmount3,
+    "category": mockCategory3
+  }
+];
 
 var Feed = React.createClass({
-  getInitialState: function() {
-    return { transactions: [] };
+  getIntialState: function() {
+    return { feedItems: [] };
   },
 
   componentDidMount: function() {
     // TODO: Pagination?
     $.ajax({
-      url: this.props.url,
+      url: '/api/transactions',
       dataType: 'json',
       success: function(transactions) {
-        this.setState({ transactions : transactions });
+        this.setState({ feedItems: transactions });
       }.bind(this),
       error: function(xhr, status, err) {
         console.error(this.props.url, status, err.toString());
       }.bind(this)
     });
+
+  },
+
+  generateFeedItems: function() {
+    if (!this.state) {
+      return;
+    }
+
+    var feedItems = this.state.feedItems.map(function(feedItem) {
+      return (
+        <li className="list-group-item"><FeedItem from={feedItem.from} to={feedItem.to} amount={feedItem.amount} category={feedItem.category} /></li>
+      );
+    });
+
+    return feedItems;
   },
 
   render: function() {
-      console.log(this.state.transactions);
     return (
       <div className="feed">
         <FeedHeader />
         <ul className="list-group">
-          {this.state.transactions.map(function(transaction) {
-            return <li className="list-group-item"><FeedItem from={transaction.from} to={transaction.to} amount={transaction.amount} category={transaction.category} /></li>;
-          })}
+          {this.generateFeedItems()}
         </ul>
       </div>
     );
