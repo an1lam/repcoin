c11a3b6b74e5f7310792eb1086a59ca7ebafecb0
Matt Ritter <Matthew.P.Ritter.15@Dartmouth.edu>
2014-10-12 15:40:04 -0400

    Update ProfileBox.jsx

--- js/components/ProfileBox.jsx
+++ js/components/ProfileBox.jsx
@@ -7,30 +7,30 @@ var $ = require('jquery');
 
 var ProfileBox = React.createClass({
   getInitialState: function() {
-    return { currentUser: {} };
+    return { user: {} };
   },
 
   componentDidMount: function() {
     $.ajax({
-      url: '/api/users/' + this.props.user,
+      url: '/api/users/' + this.props.userId,
       success: function(user) {
-        this.setState({ currentUser: user });
+        this.setState({ user: user });
       }.bind(this),
       error: function(xhr, status, err) {
-        console.error(this.props.user, status, err.toString());
+        console.error(this.props.userId, status, err.toString());
       }.bind(this)
     });
   },
   render: function() {
-    var safeLinks = this.state.currentUser.links ? this.state.currentUser.links : [];
+    var safeLinks = this.state.user.links ? this.state.user.links : [];
     var links = safeLinks.map(function(link) {
       return <div><strong>{link.title}</strong>: <a href={link.url}>{link.url}</a></div>;
     });
     return (
       <div className="profileBox">
-        <img className="profilePicture img-responsive img-thumbnail" src={this.state.currentUser.picture}></img>
+        <img className="profilePicture img-responsive img-thumbnail" src={this.state.user.picture}></img>
         <div className="profileData">
-          <h3 className="profileUsername">{this.state.currentUser.username}</h3>
+          <h3 className="profileUsername">{this.state.user.username}</h3>
           {links}
         </div>
       </div>
