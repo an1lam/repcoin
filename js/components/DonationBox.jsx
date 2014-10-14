50c5f7e417d5b4dddb66ee670ab58df0a62afe35
krazemon <stephenmalina@gmail.com>
2014-10-13 22:50:06 -0400

    Create frontend scaffolding for donation box
    - This now actually accurately deals with state
    - The next step is to create a frontend helper method and
    accompanying backend route for authenticating and generating
    new transactions

--- js/components/DonationBox.jsx
+++ js/components/DonationBox.jsx
@@ -2,28 +2,80 @@
 "use strict";
 
 var React = require('react');
+var $ = require('jquery');
 
 var DonationBox = React.createClass({
+  getInitialState: function() {
+    return {
+      error: "",
+      user: {
+        categories: []
+      }
+    };
+  },
+
+  componentDidMount: function() {
+    this.setUser('/api/users/' + this.props.userId);
+  },
+
+  componentWillReceiveProps: function(newProps) {
+    this.setUser('/api/users/' + newProps.userId);
+  },
+
+  setUser: function(url) {
+    $.ajax({
+      url: url,
+      success: function(user) {
+        this.setState({ user: user });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.userId, status, err.toString());
+      }.bind(this)
+    });
+  },
+
+  handleSubmit: function(event) {
+    event.preventDefault();
+    var reps = parseInt(this.refs.reps.getDOMNode().value);
+    var categoryName = this.refs.category.getDOMNode().value;
+    var category;
+    for (var i = 0; i < this.state.user.categories.length; i++) {
+      var currentCategory = this.state.user.categories[i];
+      if (currentCategory.name === categoryName) {
+        category = currentCategory;
+      }
+    }
+    if (category.directScore < reps) {
+      this.setState({donationError: true});
+    } else {
+      this.setState({donationError: false});
+      // TODO: Actually create a new transaction here rather than just resetting and remove the logging
+      console.log(reps);
+      console.log(category);
+    }
+    this.refs.reps.getDOMNode().value = 0;
+  },
+
   render: function() {
+    var donationError = this.state.donationError ? <p>You don't have that many reps in that category!</p> : "";
     return (
       <div className="donationBox">
         <div className="navbar navbar-default">
-        <form className="navbar-form">
+        <form onSubmit={this.handleSubmit} className="navbar-form">
           <p className="give navbar-text">Give</p>
-          <p className="user navbar-text">{this.props.user.name}</p>
-          <input type="text" className="donationAmount form-control" placeholder="10"></input>
+          <p className="user navbar-text">{this.state.user.username}</p>
+          <input type="text" ref="reps" className="donationAmount form-control" placeholder="10"></input>
             <p className="navbar-text">Reps for</p>
-            <select className="form-control donationCategories">
-              <option>Category</option>
-              <option>Coding</option>
-              <option>Jazz</option>
-              <option>Skiing</option>
+            <select ref="category" className="form-control donationCategories">            {this.state.user.categories.map(function(category) {
+                return <option value={category.name}>{category.name}</option>;
+              })}
             </select>
             <button type="submit" className="donationButton btn btn-lg btn-primary">Give</button>
-          </form>
-          </div>
+        </form>
+        {donationError}
         </div>
-      );
+      </div>
+    );
   }
 });
 
