58cc74f84c701204e065a5c3256c1e582d44bbfd
ritmatter <mritter123@gmail.com>
2014-10-19 13:14:07 -0400

    Add more transaction data and make destroying db optional

--- scripts/populateTestDB.sh
+++ scripts/populateTestDB.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-mongo reps_development --eval "db.users.remove({}); db.transactions.remove({}); db.categories.remove({});"
+#Script name: populateTestDB.sh
+#
+# Writes data from the mock json files in this directory to reps_development
+# To destroy existing data in reps_development, pass --destroy as an argument
+# ex: ./populatTestDB.sh --destroy
+
+destroy=$1
+if [ "$destroy" = "--destroy" ];
+  then
+  echo "Destroying database...";
+  mongo reps_development --eval "db.users.remove({}); db.transactions.remove({}); db.categories.remove({});"
+fi
+
 mongoimport -d reps_development -c users --jsonArray --file $REPS_ROOT/scripts/mockUserData.json
 mongoimport -d reps_development -c transactions --jsonArray --file $REPS_ROOT/scripts/mockTransactionData.json
 mongoimport -d reps_development -c categories --jsonArray --file $REPS_ROOT/scripts/mockCategoryData.json
+exit 0
