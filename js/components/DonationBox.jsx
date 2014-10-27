5ebe1e95b95e09b8baa3f7c96cb330666d3f1a67
krazemon <stephenmalina@gmail.com>
2014-10-27 16:51:47 -0400

    Factor out AJAX calls into their own functions

--- js/components/DonationBox.jsx
+++ js/components/DonationBox.jsx
@@ -19,13 +19,58 @@ var DonationBox = React.createClass({
         transactionCategory = currentCategory;
       }
     }
+    console.log(transactionCategory.reps < reps);
     if (!transactionCategory || transactionCategory.reps < reps) {
       this.setState({donationError: true});
     } else {
       this.setState({donationError: false});
-      // TODO: Actually create a new transaction here rather than just resetting and remove the logging
       this.createTransaction(this.props.user, this.props.currentUser, categoryName, reps);
     }
+  },
+
+  updateCurrentUserWithTransaction: function(toUser, fromUser, category, amount) {
+    var url = '/api/users/' + fromUser._id;
+    for (var i = 0; i < fromUser.categories.length; i++) {
+      if (fromUser.categories[i].name === category) {
+        fromUser.categories[i].reps -= amount;
+      }
+    }
+    $.ajax({
+      url: url,
+      type: 'PUT',
+      data: fromUser,
+      success: function(user) {
+        auth.storeCurrentUser(user, function(user) {
+          return user;
+        });
+        this.updateOtherUserWithTransaction(fromUser, toUser, category, amount);
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(status, err.toString());
+      }.bind(this),
+    });
+  },
+
+  updateOtherUserWithTransaction: function(fromUser, toUser, category, amount) {
+    var url = '/api/users/' + toUser._id;
+    for (var i = 0; i < toUser.categories.length; i++) {
+      if (toUser.categories[i].name === category) {
+        toUser.categories[i].directScore += amount;
+        //TODO: Figure out how to deal with crowd score. It appears this will eventually
+        // have to go on the backend
+      }
+    }
+    $.ajax({
+      url: url,
+      type: 'PUT',
+      data: toUser,
+      success: function(user) {
+        PubSub.publish('profileupdate');
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(status, err.toString());
+      }.bind(this),
+    });
   },
 
   createTransaction: function(toUser, fromUser, category, amount) {
@@ -41,46 +86,7 @@ var DonationBox = React.createClass({
         amount: amount,
       },
       success: function(transaction) {
-        var url = '/api/users/' + fromUser._id;
-        for (var i = 0; i < fromUser.categories.length; i++) {
-          if (fromUser.categories[i].name === category) {
-            fromUser.categories[i].reps -= amount;
-          }
-        }
-        $.ajax({
-          url: url,
-          type: 'PUT',
-          data: fromUser,
-          success: function(user) {
-            auth.storeCurrentUser(user, function(user) {
-              return user;
-            });
-            var url = '/api/users/' + toUser._id;
-            for (var i = 0; i < toUser.categories.length; i++) {
-              if (toUser.categories[i].name === category) {
-                toUser.categories[i].directScore += amount;
-                //TODO: Figure out how to deal with crowd score. It appears this will eventually
-                // have to go on the backend
-              }
-            }
-            $.ajax({
-              url: url,
-              type: 'PUT',
-              data: toUser,
-              success: function(user) {
-                PubSub.publish('profileupdate');
-              }.bind(this),
-              error: function(xhr, status, err) {
-                console.error(status, err.toString());
-                PubSub.publish('profileupdate');
-              }.bind(this),
-            });
-          }.bind(this),
-          error: function(xhr, status, err) {
-            console.error(status, err.toString());
-            PubSub.publish('profileupdate');
-          }.bind(this),
-        });
+        this.updateCurrentUserWithTransaction(toUser, fromUser, category, amount);
       }.bind(this),
       error: function(xhr, status, err) {
         console.error(status, err.toString());
