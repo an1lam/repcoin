a50ad3462040c89d04184cc8bc025a23629332fa
krazemon <stephenmalina@gmail.com>
2014-10-29 11:45:36 -0400

    Conditionally include reps in categories table

--- js/components/CategoriesItem.jsx
+++ js/components/CategoriesItem.jsx
@@ -5,12 +5,20 @@ var React = require('react');
 var ScoreBar = require('./ScoreBar.jsx');
 
 var CategoriesItem = React.createClass({
+  getDefaultProps: function() {
+    return {
+      reps: -1
+    }
+  },
+
   render: function() {
+    var reps = this.props.reps >= 0 ? <td>{this.props.reps}</td> : '';
     return (
       <tr className="categoriesItem">
         <td>{this.props.category}</td>
         <td><ScoreBar directRep={this.props.directRep} prevDirectRep={this.props.prevDirectRep} category={this.props.category}/></td>
         <td>{this.props.crowdRep}</td>
+        {reps}
       </tr>
     );
   }
