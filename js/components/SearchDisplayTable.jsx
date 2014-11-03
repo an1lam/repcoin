0f963e07be54ad7ea7ceb3c2b33c4368f7cce10e
ritmatter <mritter123@gmail.com>
2014-11-03 12:52:52 -0500

    Try search items

--- js/components/SearchDisplayTable.jsx
+++ js/components/SearchDisplayTable.jsx
@@ -2,6 +2,7 @@
 
 var $ = require('jquery');
 var React = require('react');
+var SearchItem = require('./SearchItem.jsx');
 
 var SearchDisplayTable = React.createClass({
   render: function() {
@@ -9,7 +10,7 @@ var SearchDisplayTable = React.createClass({
       <div className="searchDisplayTable">
         <ul className="list-group">
           {this.props.data.map(function(datum) {
-            return <li key={datum._id} className="list-group-item">{datum.username}</li>
+            return <li key={datum._id} className="list-group-item"><SearchItem datum={datum} /></li>
           })}
         </ul> 
       </div>
