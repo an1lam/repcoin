7cc8ac80207e0f1b57197091e9642d2a30750cc9
ritmatter <mritter123@gmail.com>
2014-11-09 21:08:13 -0500

    Make login work with email

--- config/pass.js
+++ config/pass.js
@@ -1,9 +1,12 @@
 var User = require('../api/models/User.js'); 
 
 module.exports = function(passport, LocalStrategy) {
-  passport.use(new LocalStrategy(
+  passport.use(new LocalStrategy({
+      usernameField: 'email',
+      passwordField: 'password',
+    },
     function(username, password, done) {
-      User.findOne({ username: username }, function(err, user) {
+      User.findOne({ email: username }, function(err, user) {
         if (err) { 
           return done(err); 
         }
