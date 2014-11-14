429f0adc963fea30217a22704b156f4956a02fc8
ritmatter <mritter123@gmail.com>
2014-11-13 20:09:50 -0500

    Create link to profile from toolbar

--- js/components/Toolbar.jsx
+++ js/components/Toolbar.jsx
@@ -12,27 +12,38 @@ var Link = Router.Link;
 var Toolbar = React.createClass({
   getInitialState: function() {
     return {
-      loggedIn: false
+      loggedIn: false,
+      currentUser: null
     };
   },
 
   componentDidMount: function() {
-    if (this.isMounted()) {
-      this.setState({ loggedIn: auth.loggedIn() });
-    }
+    this.setState({ loggedIn: auth.loggedIn() });
+    auth.getCurrentUser(function(user) {
+      this.setState({ currentUser: user });
+    }.bind(this));
   },
 
   render: function() {
-    var LogoutOrNothing = this.state.loggedIn ? <Logout /> : "";
+    var LogoutOrNothing = this.state.loggedIn ? <div className="logoutbtn"><Logout /></div> : "";
+    var profileLink = this.state.currentUser ? 
+      <div className="profilelink"><Link to="profile" params={{userId: this.state.currentUser._id}}>{this.state.currentUser.firstname}</Link></div> : '';
     return (
       <div className="toolbar navbar navbar-default" role="navigation">
         <div className="navbar-header">
-          <div className="toolbarHomeLink navbar-brand"><Link to="home">Repcoin</Link></div>
+          <div className="homelink navbar-brand">
+            <Link to="home">Repcoin</Link>
+          </div>
         </div>
         <div className="col-md-3 col-md-offset-1">
           <InstantBox />
         </div>
-        <div className="navbar navbar-right"><div className="navbar-header navbar-brand">{LogoutOrNothing}</div></div>
+        <div className="navbar navbar-right">
+          <div className="navbar-header navbar-brand">
+            {profileLink}
+            {LogoutOrNothing}
+          </div>
+        </div>
       </div>
     );
   }
