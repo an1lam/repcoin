a09459043dcb024295523920f633163789439d40
krazemon <stephenmalina@gmail.com>
2014-12-01 10:52:27 -0500

    Job for updating previousPercentiles

--- api/jobs/user.js
+++ api/jobs/user.js
@@ -4,14 +4,28 @@ module.exports = function(agenda) {
     var i = 0;
     User.find(function(err, users) {
       users.forEach(function(user) {
-        user.categories.forEach(function(category) {
+        user.portfolio.forEach(function(category) {
           category.reps += 5;
         });
         user.save();
       });
-      console.log("Updating %d users.", users.length);
+      console.log("Incrementing %d users' reps by 5.", users.length);
+    });
+  });
+
+  agenda.define('setPreviousPercentileToCurrent', function(job, done) {
+    var i = 0;
+    User.find(function(err, users) {
+      users.forEach(function(user) {
+        user.categories.forEach(function(category) {
+          category.previousPercentile = category.percentile;
+        });
+        user.save();
+      });
+      console.log("Updating %d users' previous percentiles.", users.length);
     });
   });
 
   agenda.every('0 0 * * *', 'incrementUserReps');
+  agenda.now('setPreviousPercentileToCurrent');
 };
