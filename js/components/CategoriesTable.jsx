8da7b7446df651ca3f1dca443c050ee222b826d9
ritmatter <mritter123@gmail.com>
2015-01-05 10:52:44 -0500

    Add empty table message

--- js/components/CategoriesTable.jsx
+++ js/components/CategoriesTable.jsx
@@ -79,6 +79,22 @@ var CategoriesTable = React.createClass({
     this.setState({ error: error });
   },
 
+  // Get the categories rows for the table
+  // Show reps if includeReps is true
+  getCategoriesItems: function(includeReps) {
+    var categoriesItems = [];
+    var length = this.props.user.categories.length;
+    var category;
+    for (var i = 0; i < length; i++) {
+      var category = this.props.user.categories[i];
+      categoriesItems.push(
+        <CategoriesItem key={category.id} category={category} includeReps={includeReps}
+          deleteMode={this.state.deleteMode} showDeleteBox={this.showDeleteBox} />
+      );
+    }
+    return categoriesItems;
+  },
+
   render: function() {
     var error = this.state.error ? <div className="alert alert-info" role="alert">{this.state.error}</div> : '';
     var edit = '';
@@ -116,7 +132,16 @@ var CategoriesTable = React.createClass({
     }
 
     // The key for the expert table will change if a category is deleted
-    var key = this.props.user._id + this.props.user.categories.length;
+    var length = this.props.user.categories.length;
+    var key = this.props.user._id + length;
+    var categoryRows = this.getCategoriesItems(includeReps);
+    var addCategoriesText = '';
+    if (length === 0) {
+      var text = 'You are not an expert for any categories yet! Click the "+" ' +
+        'in the top right to add some. You can create any categories that you do not find.';
+      addCategoriesText = <div className="add-category-text">{text}</div>;
+    }
+
     return (
       <div key={key} className="categoriesTable panel panel-default" onMouseOver={this.handleMouseOver} onMouseLeave={this.handleMouseLeave}>
         <CategoriesHeader user={this.props.user} />
@@ -132,12 +157,10 @@ var CategoriesTable = React.createClass({
               <th>Top Investors</th>
               {repsHeader}
             </tr>
-            {this.props.user.categories.map(function(category) {
-              return <CategoriesItem key={category.id} category={category} includeReps={includeReps}
-                deleteMode={this.state.deleteMode} showDeleteBox={this.showDeleteBox} />
-            }.bind(this))}
+            {categoryRows}
           </tbody>
         </table>
+        <p>{addCategoriesText}</p>
       </div>
     );
   }
