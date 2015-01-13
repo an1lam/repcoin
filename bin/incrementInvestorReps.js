3f5755f7349db3450696251b247f747bc82408f8
ritmatter <mritter123@gmail.com>
2015-01-12 23:47:42 -0500

    Build increment user reps. Fix update percentiles

--- bin/incrementInvestorReps.js
+++ bin/incrementInvestorReps.js
@@ -7,17 +7,18 @@ var winston = require('winston');
 // Set up the database url
 var db = require('../config/db');
 if (process.env.NODE_ENV === 'production') {
-  winston.log('info', 'Starting updatePercentiles in production environment');
+  winston.log('info', 'Starting increment investor reps in production environment');
   var mongoURL = db.production_url;
 } else {
-  winston.log('info', 'Starting updatePercentiles in development environment');
+  winston.log('info', 'Starting increment investor reps in development environment');
   var mongoURL = db.development_url;
 }
+
 mongoose.connect(mongoURL);
 mongoose.connection.on('connected', function() {
-  utils.setPreviousPercentileToCurrent(function(errs) {
+  utils.incrementInvestorReps(function(errs) {
     if (errs) {
-      winston.log('info', 'Update percentiles finished with errors.');
+      winston.log('info', 'Increment investor reps finished with errors.');
     }
     mongoose.connection.close();
     process.exit();
