4f69aaa371cd64b476e37e73a251723559c69992
ritmatter <mritter123@gmail.com>
2015-01-13 15:48:24 -0500

    Create quick view card for profile viewing on home page

--- js/components/HomePage.jsx
+++ js/components/HomePage.jsx
@@ -1,21 +1,32 @@
-/** @jsx React.DOM */
-"use strict";
+'use strict';
 
+var AuthenticatedRoute = require('../mixins/AuthenticatedRoute.jsx');
+var CategoriesTable = require('./CategoriesTable.jsx');
+var Feed = require('./Feed.jsx');
+var Footer = require('./Footer.jsx');
+var ProfileQuickView = require('./ProfileQuickView.jsx');
 var React = require('react');
 var Toolbar = require('./Toolbar.jsx');
-var Footer = require('./Footer.jsx');
-var Feed = require('./Feed.jsx');
-var CategoriesTable = require('./CategoriesTable.jsx');
-var AuthenticatedRoute = require('../mixins/AuthenticatedRoute.jsx');
 
 var HomePage = React.createClass({
   mixins: [AuthenticatedRoute],
   render: function() {
     return (
       <div className="homePage">
-        <Toolbar />
-        <div className="col-md-8 col-md-offset-2"><Feed parent="HomePage" /></div>
-        <Footer />
+        <div className="row">
+          <Toolbar />
+        </div>
+        <div className="row">
+          <div className="col-md-3">
+            <ProfileQuickView />
+          </div>
+          <div className="col-md-6">
+            <Feed parent="HomePage" />
+          </div>
+        </div>
+        <div className="row footerrow">
+          <Footer />
+        </div>
       </div>
     );
   }
