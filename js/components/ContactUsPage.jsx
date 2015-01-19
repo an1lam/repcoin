162641a9673982b0a650ddd8901d7e981477b0d0
ritmatter <mritter123@gmail.com>
2015-01-19 16:52:50 -0500

    Include link to google forum on signup page

--- js/components/ContactUsPage.jsx
+++ js/components/ContactUsPage.jsx
@@ -13,11 +13,12 @@ var ContactUsPage = React.createClass({
         <div className="container-fluid">
           <p>If you have questions, comments or concerns, please email&nbsp;
           <a href="mailto:stephenmalina@gmail.com?Subject=Reps%20Contact" target="_top">
-            Stephen</a> 
+            Stephen</a>
           &nbsp;or&nbsp;
           <a href="mailto:mritter123@gmail.com?Subject=Reps%20Contact" target="_top">
             Matt</a>&nbsp;
           and we will do our best to get back to you ASAP.</p>
+          <p>In addition, if you have any comments on the site or improvements, please suggest them on our Google Forum <a href="https://groups.google.com/forum/#!forum/repcoin-feedback">here</a>.</p>
         </div>
         <Footer />
       </div>
