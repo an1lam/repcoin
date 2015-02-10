86b3d62ddc5a475cf1922e3cad3e4bdccdc41328
ritmatter <mritter123@gmail.com>
2015-02-09 20:38:54 -0500

    Make it possible to use fb login on dev

--- config/pass.js
+++ config/pass.js
@@ -48,12 +48,13 @@ module.exports = function(passport, LocalStrategy, FacebookTokenStrategy) {
 
         // TODO: remove this once we are in Beta
         // This hack allows us to login with facebook
-
-        if (!user) {
-          return done('Sorry, Repcoin is closed until the Beta release');
-        }
-        if (user.facebookId !== process.env.MATT_RITTER_FACEBOOK_ID && user.facebookId !== process.env.STEPHEN_MALINA_FACEBOOK_ID)  {
-          return done('Closed until beta release');
+        if (process.env.NODE_ENV === 'production') {
+          if (!user) {
+            return done('Sorry, Repcoin is closed until the Beta release');
+          }
+          if (user.facebookId !== process.env.MATT_RITTER_FACEBOOK_ID && user.facebookId !== process.env.STEPHEN_MALINA_FACEBOOK_ID)  {
+            return done('Sorry, Repcoin is closed until the Beta release');
+          }
         }
 
         // If the user does not exist, then create it
