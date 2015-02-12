5c9075d96a0c1c44831889786fdbcc23a7a8ce08
Stephen Malina <stephenmalina@gmail.com>
2015-02-12 11:22:03 -0500

    Fix team page links

--- js/components/TeamPage.jsx
+++ js/components/TeamPage.jsx
@@ -13,7 +13,7 @@ var TeamPage = React.createClass({
         </div>
         <div className="row col-md-8 col-md-offset-1 team-content">
           <h1>The Team</h1>
-          <p>{strings.TEAM_PART_1A} <a href="http://krazemon.github.io/">Stephen Malina</a> and <a href="https://www.linkedin.com/profile/public-profile-settings?trk=prof-edit-edit-public_profile">Matt Ritter</a>. {strings.TEAM_PART_1B}</p>
+          <p>{strings.TEAM_PART_1A} <a target='_blank' href="http://krazemon.github.io/">Stephen Malina</a> and <a target='_blank' href="https://www.linkedin.com/pub/matthew-ritter/46/b2a/b72">Matt Ritter</a>. {strings.TEAM_PART_1B}</p>
           <img className="team-picture" src={strings.TEAM_PICTURE} />
           <p>{strings.TEAM_PART_2}</p>
           <p>{strings.TEAM_PART_3}</p>
