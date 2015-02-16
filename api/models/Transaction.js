74cfd98b75c9277b80fbc15761b164a347ad86c3
ritmatter <mritter123@gmail.com>
2015-02-16 17:10:43 -0500

    Leading should only have active experts

--- api/models/Transaction.js
+++ api/models/Transaction.js
@@ -59,7 +59,6 @@ TransactionSchema.statics.findTrendingExperts = function(timeStamp, category) {
     { $project: { "to.id": 1 } },
     { $group: { _id: "$to.id", count: {$sum: 1} }  },
     { $sort: { "count": -1 } },
-    { $limit: 10 }
   ]).exec();
 };
 
