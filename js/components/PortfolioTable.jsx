9ab632be0cb44cbcb3c6f5c59a87a3e6b9e7d708
Stephen Malina <stephenmalina@gmail.com>
2015-02-21 14:57:11 -0500

    Fix Package.json and fix gettotaldividends

--- js/components/PortfolioTable.jsx
+++ js/components/PortfolioTable.jsx
@@ -34,6 +34,10 @@ var PortfolioTable = React.createClass({
       if (!category.investments) {
         continue;
       }
+<<<<<<< HEAD
+=======
+
+>>>>>>> 47beec2... Fix getotaldividends
       for (var j = 0; j < category.investments.length; j++) {
         totalDividends += category.investments[j].dividend;
       }
