a18af15ea3a96c480ad7e08c3892d18c8b43cc5d
Stephen Malina <stephenmalina@gmail.com>
2015-03-04 00:14:24 -0500

    Update DB URI

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 // config/db.js
   module.exports = {
-    production_url: 'mongodb://heroku_app31747026:rn8dbslkp4k15ne2aoc97139ua@ds053370.mongolab.com:53370/heroku_app31747026',
+    production_url: 'mongodb://admin:' + process.env.DB_PASSWORD + '@ds051681-a0.mongolab.com:51681/heroku_app31747026',
     development_url: 'mongodb://localhost/reps_development',
     test_url: 'mongodb://localhost/reps/test'
   }
